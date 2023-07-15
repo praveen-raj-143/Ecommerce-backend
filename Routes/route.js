e6e8b0c95b2ac844a793b9bdd7951d62f8d53be5
praveen-raj-143 <praveenrajgreath@gmail.com>
2023-07-15 14:02:25 +0530

    modified

--- Routes/route.js
+++ Routes/route.js
@@ -2,7 +2,7 @@ const route=require("express").Router()
 
 const controller = require("../Controller/action");
 
-route.get("/",controller.allproduct)
+route.get("/accessories",controller.allproduct)
 // route.get("/cart",controller.cart)
 // route.post("/signup",controller.signup)
 module.exports=route;
